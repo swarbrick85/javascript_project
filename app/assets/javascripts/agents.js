63674f9f11a9cced9efbc2e7d81ed574f6dc4392
github username <github email address>
2019-08-05 00:55:58 -0400

    agents index works

--- app/assets/javascripts/agents.js
+++ app/assets/javascripts/agents.js
@@ -3,13 +3,44 @@ $(() => {
 })
 
 const agents = () => {
+  let userId
   $('.agents').on('click', (e) => {
     e.preventDefault()
-    console.log('hello')
     fetch(`${$(".agents").attr("href")}` + `.json`)
       .then(res => res.json())
       .then(data => {
-        $('#app-container').html('hellot')
+          $('#app-container').html('<h1>Your Agents</h1><br><br>')
+          Object.values(data)[0].forEach(agent => {
+          let newAgent = new Agent(agent)
+          let postHtml = newAgent.formatIndex()
+          userId = newAgent.user_id
+          $('#app-container').append(postHtml)
+
+        })
+        $('#app-container').append(`
+          <br>
+          <a href="/users/${userId}/agents/new">Create a New Agent</a><br>
+          <a href="/users/${userId}">Your Home Page</a><br>
+          `)
       })
   })
+}
+
+function Agent(agent) {
+  this.name = agent["attributes"]["name"]
+  this.country_of_origin = agent["attributes"]["country-of-origin"]
+  this.id = agent["id"]
+  this.user_id = agent["attributes"]["user-id"]
+  this.players = agent["relationships"]["players"]
+  this.clubs = agent["relationships"]["clubs"]
+  this.contracts = agent["relationships"]["contracts"]
+  this.user = agent["relationships"]["user"]
+}
+
+Agent.prototype.formatIndex = function() {
+  let postHtml = `
+  <a href="/users/${this.user_id}/agents/${this.id}">${this.name}</a><br>
+  `
+
+  return postHtml
 }
